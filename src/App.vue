4e5f0cd726db130f6c34abb9e579d0c5bdc250d7
yyq-bit <174586003@qq.com>
2022-06-29 15:40:02 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
